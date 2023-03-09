32ddd8c7b1a59297c9ba5efcc45705e154c3da02
ingridsterphane <ingridsterphane@hotmail.com>
2023-03-09 18:42:56 -0300

    Update

---
+++ list/ExemploList.java
@@ -0,0 +1,21 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ExemploList {
+    public static void main (String[] args) {
+
+        List<Double> notas = new ArrayList<>();
+        System.out.println("Crie uma lista e adicione as sete notas: ");
+        notas.add(7.0);
+        notas.add(7.0);
+        notas.add(7.0);
+        notas.add(7.0);
+        notas.add(7.0);
+        notas.add(7.0);
+        notas.add(7.0);
+
+    }
+
+}
