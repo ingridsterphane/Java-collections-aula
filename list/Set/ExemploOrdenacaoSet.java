876ebb16ece8fe323076017eec5eaf6686e5cf77
ingridsterphane <ingridsterphane@hotmail.com>
2023-03-11 18:07:38 -0300

    Update

--- list/Set/ExemploOrdenacaoSet.java
+++ list/Set/ExemploOrdenacaoSet.java
@@ -1,7 +1,9 @@
 package list.Set;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class ExemploOrdenacaoSet {
     public static void main (String[] args) {
@@ -14,6 +16,28 @@ public class ExemploOrdenacaoSet {
         }};
         for (Serie serie: minhasSeries) System.out.println(serie.getNome() + " - "
         + serie.getGenero() + " - " + serie.getTempoEpisodio());
+
+        System.out.println("--\tOrdem de Inserção\t---");
+        Set<Serie> minhasSeries1 = new LinkedHashSet<>() {{
+            add(new Serie("GOT", "Fantasia", 60));
+            add(new Serie("Run BTS", "Variedades", 80));
+            add(new Serie("The Glory", "Drama", 90));
+        }};
+        for (Serie serie: minhasSeries1) System.out.println(serie.getNome() + " - "
+                + serie.getGenero() + " - " + serie.getTempoEpisodio());
+
+        System.out.println("--\tOrdem Natural (Tempo Episodio)\t---");
+        Set<Serie> minhasSeries2 = new TreeSet<>(minhasSeries1);
+        for (Serie serie: minhasSeries2) System.out.println(serie.getNome() + " - "
+                + serie.getGenero() + " - " + serie.getTempoEpisodio());
+
+        System.out.println("--\tOrdem Nome/Gênero/TempoEpisodio\t---");
+        Set<Serie> minhasSeries3 = new TreeSet<>(new ComparatorNomeGeneroTempoEpisodio());
+        minhasSeries3.addAll(minhasSeries);
+        for (Serie serie: minhasSeries3) System.out.println(serie.getNome() + " - "
+                + serie.getGenero() + " - " + serie.getTempoEpisodio());
+
+
 
     }
 }
--- list/Set/classOrdenacaoSet.java
+++ list/Set/classOrdenacaoSet.java
@@ -1,12 +1,13 @@
 package list.Set;
 
+import java.util.Comparator;
 import java.util.Objects;
 
 public class classOrdenacaoSet {
 
 
 }
-class Serie {
+class Serie implements Comparable<Serie> {
     private String nome;
     private String genero;
     private Integer tempoEpisodio;
@@ -49,5 +50,28 @@ class Serie {
     @Override
     public int hashCode() {
         return Objects.hash(nome, genero, tempoEpisodio);
+    }
+
+    @Override
+    public int compareTo(Serie serie) {
+        /* Se o tempo for igual :   OBS: != sig. diferente de zero
+        int tempoEpisodio = Integer.compare(this.getTempoEpisodio(), serie.getTempoEpisodio());
+        if (tempoEpisodio != 0) return tempoEpsodio;
+        se não
+        return this.Getgenero = compareTo(serie.getGenero());
+         */
+        return Integer.compare(this.getTempoEpisodio(), serie.getTempoEpisodio());
+    }
+}
+class ComparatorNomeGeneroTempoEpisodio implements Comparator<Serie>{
+    @Override
+    public int compare(Serie s1, Serie s2) {
+        int nome =s1.getNome() .compareTo(s2.getNome());
+        if (nome != 0) return nome;
+
+        int genero =s1.getGenero() .compareTo(s2.getGenero());
+        if (genero != 0) return genero;
+
+        return Integer.compare(s1.getTempoEpisodio(), s2.getTempoEpisodio());
     }
 }
