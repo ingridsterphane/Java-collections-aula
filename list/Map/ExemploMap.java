20b51e1ce64cbf1eb700ea9c94d74cab3ef7d3d7
ingridsterphane <ingridsterphane@hotmail.com>
2023-03-14 17:09:15 -0300

    Update

--- list/Map/ExemploMap.java
+++ list/Map/ExemploMap.java
@@ -3,10 +3,10 @@ package list.Map;
 import java.util.*;
 
 public class ExemploMap {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         System.out.println("Crie um dicionário que relacione os modelos e seus respectivos consumos: ");
-        Map<String,Double> carrosPopulares = new HashMap<>(){{
+        Map<String, Double> carrosPopulares = new HashMap<>() {{
             put("Gol", 14.4);
             put("Uno", 15.6);
             put("Mobi", 16.1);
@@ -19,7 +19,7 @@ public class ExemploMap {
         carrosPopulares.put("Gol", 15.2);
         System.out.println(carrosPopulares);
 
-        System.out.println("Confira se o modelo tucson está no dicionário: " +carrosPopulares.containsKey("tucson"));
+        System.out.println("Confira se o modelo tucson está no dicionário: " + carrosPopulares.containsKey("tucson"));
 
         System.out.println("Exiba o consumo do Uno: " + carrosPopulares.get("Uno"));
 
@@ -37,11 +37,31 @@ public class ExemploMap {
         Set<Map.Entry<String, Double>> entries = carrosPopulares.entrySet();
         String modeloMaisEficiente = "";
 
-        for (Map.Entry<String, Double> entry:entries){
+        for (Map.Entry<String, Double> entry : entries) {
             if (entry.getValue().equals(consumoMaisEficiente)) {
                 modeloMaisEficiente = entry.getKey();
-                System.out.println("Modelo mais eficiente: " + modeloMaisEficiente + "-" +consumoMaisEficiente);
+                System.out.println("Modelo mais Eficiente: " + modeloMaisEficiente + "-" + consumoMaisEficiente);
             }
         }
+
+        System.out.println("Exiba o modelo menos econômico e seu consumo: ");
+
+        Double consumoMenosEficiente = Collections.min(carrosPopulares.values());
+        String modeloMenosEficiente = "";
+        for (Map.Entry<String, Double> entry : carrosPopulares.entrySet()) {
+            if (entry.getValue().equals(consumoMenosEficiente)) {
+                modeloMenosEficiente = entry.getKey();
+                System.out.println("Modelo Menos Eficiente " + modeloMenosEficiente + "-" + consumoMenosEficiente);
+            }
+
+        }
+
+
+        Iterator<Double> iterator = carrosPopulares.values().iterator();
+        Double soma = 0d;
+        while (iterator.hasNext()) {
+            soma += iterator.next();
+        }
+        System.out.println("Exiba a soma dos consumos: " + soma);
     }
 }
